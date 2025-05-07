10f99f38ed0281f4af9e7f1f0ea861590b2f46f7
Kenneth Siyabonga Ncube <160495796+ghost2468developer@users.noreply.github.com>
2025-05-07 13:30:59 +0200

    set up

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const authRoutes = require('./routes/auth')
+const stockRoutes = require('./routes/stock')
+
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+
+app.use('./api/auth', authRoutes)
+app.use('./api/stock', stockRoutes)
+
+module.exports = app
